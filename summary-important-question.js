869c785bc43cd27301ae28ba1550363bdfd94e0b
abdullahalmarup1 <abdullahalmarup49@.com>
2023-08-04 19:49:57 +0600

    summary

---
+++ summary-important-question.js
@@ -0,0 +1,42 @@
+// ey jinis gulu must jante hobe
+
+/* 
+variable related
+-------------------------
+
+1.what is javasript
+2.how does js works
+3.what is variable
+4.declare a variable
+5.types of variable , how can you find out a type of variable
+6.primitive and non primitive data types
+7.naming convention of js variables
+8.math operations +,-,*,/,%
+9.short hand: +=,-=,*=,/=
+10.++,--
+11.parseInt,parseFloat
+12.toFixed
+*/
+
+// Array related
+// -----------------------
+
+/* 
+1.what is the purpose
+2.how to declare an array in js
+3.number of element an array
+4.what is index
+5.find the value of an element by index
+6.change an element by index
+7.get the index of an element by the value
+8.how can you add an element to an array at the last position
+8.how can you remove an element to an array at the last position
+8.how can you add an element to an array at the first position
+8.how can you remove an element to an array at the first position
+
+
+
+baki gulu likhi nai (module-19)
+
+
+*/
